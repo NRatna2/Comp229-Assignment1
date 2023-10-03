59687b4cf994d01664c5a9f48e9a87fae3d5f088
NRatna2 <nahida.ratna99@gmail.com>
2023-10-03 00:48:45 -0400

    node_module

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.set('view engine','ejs');
 
 app.use(express.static('public'));
-app.use(express.static('project'));
+
 
 app.get('/', (req,res)=>{
     res.render('pages/home');
@@ -30,10 +30,7 @@ app.get('/contact', (req,res)=>{
     res.render('pages/contact');
 });
 
-app.get('/project/:projectName', (req, res) => {
-    const projectName = req.params.projectName;
-    res.sendFile(path.join(__dirname, `project/${projectName}/index.html`));
-  });
+
 
 
 app.listen(4000);
